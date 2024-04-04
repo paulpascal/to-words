9cc7a184f1641b86e6f79932c0f3b02fd1671721
conqlabo <124415862+conqlabo@users.noreply.github.com>
2024-04-04 15:18:55 +0000

    feat: add more currencies part 4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,22 +6,25 @@ describe('toWords', () => {
     expect(() => toWords(123.45).toEqual('One Hundred Twenty Three Point Fourty Five'));
   });
 
-  test(`EUR`, (done) => {
-    setTimeout(() => {
-      expect(() => toWords(
-          452, {currency: true, label: 'EUR'}
-      ).toEqual('Four Hundred Fifty Two EUR Only'));
-      done(); // Call done to indicate the end of the test
-    }, 140000); // 4 minutes delay (4 * 60 * 1000 milliseconds)
-  });
+  for (let i = 0; i < 16; i++) {
+    test(`EUR ${i}`, (done) => {
+      setTimeout(() => {
+        expect(() => toWords(
+            452, {currency: true, label: 'EUR'}
+        ).toEqual('Four Hundred Fifty Two EUR Only'));
+        done(); // Call done to indicate the end of the test
+      }, 4500); // 4500 milliseconds delay
+    });
+  }
 
-  test(`EUR`, (done) => {
-    setTimeout(() => {
-      expect(() => toWords(
-          452.36, {currency: true, label: 'EUR'}
-      ).toEqual('Four Hundred Fifty Two EUR And Thirty Six Only'));
-      done(); // Call done to indicate the end of the test
-    }, 140000); // 4 minutes delay (4 * 60 * 1000 milliseconds)
-  });
+  for (let i = 0; i < 16; i++) {
+    test(`EUR ${i}`, (done) => {
+      setTimeout(() => {
+        expect(() => toWords(
+            452.36, {currency: true, label: 'EUR'}
+        ).toEqual('Four Hundred Fifty Two EUR And Thirty Six Only'));
+        done(); // Call done to indicate the end of the test
+      }, 4500); // 4500 milliseconds delay
+    });
+  }
 });
-
