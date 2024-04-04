91c1b8e51e8c00eaa7448e20d6d7c7ca52b6beba
conqlabo <124415862+conqlabo@users.noreply.github.com>
2024-04-04 15:39:32 +0000

    Update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,7 +6,7 @@ describe('toWords', () => {
     expect(() => toWords(123.45).toEqual('One Hundred Twenty Three Point Fourty Five'));
   });
 
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < 28; i++) {
     test(`EUR ${i}`, (done) => {
       setTimeout(() => {
         expect(() => toWords(
