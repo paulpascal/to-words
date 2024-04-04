713de69bd2e76f208d43f40eee8e6e4279e720aa
conqlabo <124415862+conqlabo@users.noreply.github.com>
2024-04-04 13:19:23 +0000

    feat: add support for others currency
    Add support for others currencies PART 1

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,4 +5,8 @@ describe('toWords', () => {
     expect(() => toWords(123).toEqual('One Hundred Twenty Three'));
     expect(() => toWords(123.45).toEqual('One Hundred Twenty Three Point Fourty Five'));
   });
+
+  test(`EUR`, () => {
+    expect(() => toWords(452, { currency: true, label: 'EUR' }).toEqual('Four Hundred Fifty Two EUR Only'));
+  });
 });
